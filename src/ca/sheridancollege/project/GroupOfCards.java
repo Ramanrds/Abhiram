36196a25fcb9196cd37e596fde64f2f33e1cbdc4
Ramanrds <sing8546@sheridancollege.ca>
2019-08-12 14:30:13 -0400

    SYST Commit3

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -5,53 +5,114 @@
  */
 package ca.sheridancollege.project;
 
+
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+
 
 /**
- * A concrete class that represents any grouping of cards for a Game.
- * HINT, you might want to subclass this more than once.
- * The group of cards has a maximum size attribute which is flexible for reuse.
- * @author dancye
+ *This class defines how game will be played
+ 
  */
-public class GroupOfCards 
-{
-   
-    //The group of cards, stored in an ArrayList
-    private ArrayList <Card> cards;
-    private int size;//the size of the grouping
+public  class GroupOfCards  extends WarGame{
     
-    public GroupOfCards(int givenSize)
+    public void playCard(LinkedList<Card> deckCPU,LinkedList<Card> deckP1,String player1)
     {
-        size = givenSize;
-    }
-    
-    /**
-     * A method that will get the group of cards as an ArrayList
-     * @return the group of cards.
-     */
-    public ArrayList<Card> showCards()
-    {
-        return cards;
-    }
-    
-    public void shuffle()
-    {
-        Collections.shuffle(cards);
+        int size = deckCPU.size();
+        int size1=deckP1.size();
+        
+        boolean res=false;
+        res=checkShuffle(size,size1);
+        if(res){
+            System.out.println("Error: The size of shuffled deck is not same.");
+        }else{
+            //creating card objects to compare the values later on
+            Card p1Card = deckCPU.pop(); 
+            Card p2Card = deckP1.pop();
+            
+            //display the face up card
+            System.out.println(player1 + "\'s card:  " + p1Card.toString());
+            System.out.println("Computer's card: " + p2Card.toString());
+            
+            //Number comparation between two cards
+            if(p1Card.getValue() > p2Card.getValue()){                          
+                deckCPU.addLast(p1Card);                                              
+                deckCPU.addLast(p2Card);                                               
+                System.out.println(player1 + " Wins\n");
+            }
+ 
+            else if(p1Card.getValue() < p2Card.getValue()){                    
+                deckP1.addLast(p1Card);   
+                deckP1.addLast(p2Card);  
+                System.out.println("Computer Wins\n");
+            }
+            
+            else {                                         
+                System.out.println("war"); 
+                
+                //creating war cards
+                List<Card> war1 = new ArrayList<Card>(); 
+                List<Card> war2 = new ArrayList<Card>();
+                
+                //checking if players have enough (4)cards 
+                for(int x=0; x<3; x++){ 
+                    
+                    //if one player does not have enough cards then he loses automatically
+                    
+                    if(deckCPU.size() == 0 || deckP1.size() == 0 ){                      
+                        break;
+                    }
+                    
+                    
+                    System.out.println("War card for " +player1+ " is |_|\nWar card for Computer is |_|");
+
+                    war1.add(deckCPU.pop());  
+                    war2.add(deckP1.pop());                  
+                }
+                
+              
+                if(war1.size() == 3 && war2.size() == 3 ){
+                    
+                    
+                    System.out.println("War card for " +player1 +" is " + war1.get(0).toString());
+                    System.out.println("War card for Computer is " + war2.get(0).toString());
+                    
+                    //if player 1 wins the war
+                    if(war1.get(2).getValue() > war2.get(2).getValue()){
+                        deckCPU.addAll(war1); //player1 get all cards
+                        deckCPU.addAll(war2);
+                        System.out.println("Winner of The War: "+player1);
+                    }//end if
+                    // player 2 wins war
+                    else{
+                        deckP1.addAll(war1); //player2 get all cards
+                        deckP1.addAll(war2);
+                        System.out.println("Winner of the War: Computer");
+                    }                      
+                }
+                
+            }            
+            if(deckCPU.size() == 0 ){
+                System.out.println("............................GAME OVER............................\nWinner: "+ player1);
+               
+            }
+            else if(deckP1.size() == 0){
+                System.out.println("............................GAME OVER............................\nWinner: Computer.");
+               
+            }
+           
+        } //end of while loop
     }
 
-    /**
-     * @return the size of the group of cards
-     */
-    public int getSize() {
-        return size;
+    public boolean checkShuffle(int size, int size1) {
+        if(size==size1){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
-
-    /**
-     * @param givenSize the max size for the group of cards
-     */
-    public void setSize(int givenSize) {
-        size = givenSize;
-    }
-    
-}//end class
+        
+}
