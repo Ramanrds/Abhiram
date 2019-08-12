0ea4d4884aa17e3033bbacb3eddc04673e1d9423
Ramanrds <sing8546@sheridancollege.ca>
2019-08-12 14:30:29 -0400

    SYST Commit3

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -2,6 +2,7 @@
  * SYST 17796 Project Winter 2019 Base code.
  * Students can modify and extend to implement their game.
  * Add your name as a modifier and the date!
+ * modified by group-Abhiram
  */
 package ca.sheridancollege.project;
 
@@ -9,40 +10,18 @@ package ca.sheridancollege.project;
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  * @author dancye, 2018
  */
-public abstract class Player 
-{
-    private String playerID; //the unique ID for this player
+public class Player {
     
-    /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name)
-    {
-        playerID= name;
-    }
+    //field variable
+    private String playerName;
     
-    /**
-     * @return the playerID
-     */
-    public String getPlayerID() 
-    {
-        return playerID;
+    public void setName(String newPlayerName){
+        playerName=newPlayerName;
+    }    
+    
+    public String getName(){
+        return playerName;
     }
 
-    /**
-     * Ensure that the playerID is unique
-     * @param givenID the playerID to set
-     */
-    public void setPlayerID(String givenID) 
-    {
-        playerID = givenID;
-    }
     
-    /**
-     * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
-     */
-    public abstract void play();
-    
-}
+}//end class 
